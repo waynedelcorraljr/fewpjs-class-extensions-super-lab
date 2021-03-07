1188973ef486912ed19327e1bd907c4c239defb8
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-03-06 21:32:58 -0600

    commit tree cc6f9820238bc050000a0e83c74902150c2a945d

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Deciduous extends Tree {
     }
 
     static definition() {
-        return `${super.definition()}` + ' Deciduous trees shed their leaves annually.'
+        return super.definition() + ' Deciduous trees shed their leaves annually.'
     }
 }
 
